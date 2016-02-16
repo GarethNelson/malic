ad87ab15e7040939ed626f1fe63414a74678a856
Gareth Nelson <gareth@garethnelson.com>
2016-02-16 04:03:00 +0000

    Missing files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A simple Darwin distro
+
+Generally you'll need to build this from MacOS X for now, to do so run macos-build.sh
--- macos-build.sh
+++ macos-build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+mkdir build/
+
 cd dtrace-118.1/
 mkdir obj sym dst
 xcodebuild install -target ctfconvert -target ctfdump -target ctfmerge ARCHS="x86_64" SRCROOT=$PWD OBJROOT=$PWD/obj SYMROOT=$PWD/sym DSTROOT=$PWD/dst
 sudo ditto dst/usr/local /usr/local
 cd ..
 
+cd xnu-3248.20.55/
+make TARGET_CONFIGS="RELEASE X86_64 NONE" SDKROOT=macosx all
+cd ..
